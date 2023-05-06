1590a37f92ee70f6c227e0fb8ccd0165962d03ae
Crowdbotics <team@crowdbotics.com>
2023-05-06 16:06:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rapid-leaf-40495",
+  url: "https://rapid-leaf-40495.botics.co",
+  api: "https://rapid-leaf-40495.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
